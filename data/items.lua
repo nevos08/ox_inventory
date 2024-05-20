e363a94e2c9793d540858f1d4dab06f09b3d9ea6
Le0n <86106048+nevos08@users.noreply.github.com>
2024-05-20 19:16:44 +0200

    Update stuff

--- data/items.lua
+++ data/items.lua
@@ -53,21 +53,6 @@ return {
 		consume = 0.3
 	},
 
-	['bandage'] = {
-		label = 'Bandage',
-		weight = 115,
-		client = {
-			anim = { dict = 'missheistdockssetup1clipboard@idle_a', clip = 'idle_a', flag = 49 },
-			prop = { model = `prop_rolled_sock_02`, pos = vec3(-0.14, -0.14, -0.08), rot = vec3(-50.0, -50.0, 0.0) },
-			disable = { move = true, car = true, combat = true },
-			usetime = 2500,
-		}
-	},
-
-	['black_money'] = {
-		label = 'Dirty Money',
-	},
-
 	['burger'] = {
 		label = 'Burger',
 		weight = 220,
@@ -78,64 +63,6 @@ return {
 			usetime = 2500,
 			notification = 'You ate a delicious burger'
 		},
-	},
-
-	['sprunk'] = {
-		label = 'Sprunk',
-		weight = 350,
-		client = {
-			status = { thirst = 200000 },
-			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
-			prop = { model = `prop_ld_can_01`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
-			usetime = 2500,
-			notification = 'You quenched your thirst with a sprunk'
-		}
-	},
-
-	['parachute'] = {
-		label = 'Parachute',
-		weight = 8000,
-		stack = false,
-		client = {
-			anim = { dict = 'clothingshirt', clip = 'try_shirt_positive_d' },
-			usetime = 1500
-		}
-	},
-
-	['garbage'] = {
-		label = 'Garbage',
-	},
-
-	['paperbag'] = {
-		label = 'Paper Bag',
-		weight = 1,
-		stack = false,
-		close = false,
-		consume = 0
-	},
-
-	['identification'] = {
-		label = 'Identification',
-		client = {
-			image = 'card_id.png'
-		}
-	},
-
-	['panties'] = {
-		label = 'Knickers',
-		weight = 10,
-		consume = 0,
-		client = {
-			status = { thirst = -100000, stress = -25000 },
-			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
-			prop = { model = `prop_cs_panties_02`, pos = vec3(0.03, 0.0, 0.02), rot = vec3(0.0, -13.5, -1.5) },
-			usetime = 2500,
-		}
-	},
-
-	['lockpick'] = {
-		label = 'Lockpick',
-		weight = 160,
 	},
 
 	['phone'] = {
@@ -162,18 +89,6 @@ return {
 		label = 'Money',
 	},
 
-	['mustard'] = {
-		label = 'Mustard',
-		weight = 500,
-		client = {
-			status = { hunger = 25000, thirst = 25000 },
-			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
-			prop = { model = `prop_food_mustard`, pos = vec3(0.01, 0.0, -0.07), rot = vec3(1.0, 1.0, -1.5) },
-			usetime = 2500,
-			notification = 'You.. drank mustard'
-		}
-	},
-
 	['water'] = {
 		label = 'Water',
 		weight = 500,
@@ -185,41 +100,5 @@ return {
 			cancel = true,
 			notification = 'You drank some refreshing water'
 		}
-	},
-
-	['radio'] = {
-		label = 'Radio',
-		weight = 1000,
-		stack = false,
-		allowArmed = true
-	},
-
-	['armour'] = {
-		label = 'Bulletproof Vest',
-		weight = 3000,
-		stack = false,
-		client = {
-			anim = { dict = 'clothingshirt', clip = 'try_shirt_positive_d' },
-			usetime = 3500
-		}
-	},
-
-	['clothing'] = {
-		label = 'Clothing',
-		consume = 0,
-	},
-
-	['mastercard'] = {
-		label = 'Fleeca Card',
-		stack = false,
-		weight = 10,
-		client = {
-			image = 'card_bank.png'
-		}
-	},
-
-	['scrapmetal'] = {
-		label = 'Scrap Metal',
-		weight = 80,
 	},
 }
